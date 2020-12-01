79996b248e02f2224897990a99c6677a7b630ba8
Andrew Stewart <andrew@stwrt.ca>
2020-11-30 21:41:14 -0800

    Add data/solutions for Day 01

--- lib/day_01/day_01.ex
+++ lib/day_01/day_01.ex
@@ -8,17 +8,37 @@ defmodule Day01 do
 
   @data [__DIR__, "DATA"]
         |> Path.join()
-        |> File.read!()
+        |> File.stream!()
+        |> Stream.map(&String.trim/1)
+        |> Enum.map(&String.to_integer/1)
 
   def data do
     @data
   end
 
+  # find permutation of two inputs which sums to 2020, multiply for answer
   def part1 do
-    data()
+    all_nums = data()
+
+    Enum.reduce_while(all_nums, :ok, fn current, _ ->
+      case Enum.find(all_nums -- [current], & &1 + current == 2020) do
+        nil -> {:cont, :ok}
+        counterpart -> {:halt, current * counterpart}
+      end
+    end)
   end
 
+  # find permutation of three inputs which sums to 2020, multiply for answer
   def part2 do
-    data()
+    all_nums = data()
+
+    Enum.reduce_while(all_nums, :ok, fn alpha, _ ->
+      Enum.reduce_while(all_nums -- [alpha], :ok, fn bravo, _ ->
+        case Enum.find(all_nums -- [alpha, bravo], & &1 + alpha + bravo == 2020) do
+          nil -> {:cont, {:cont, :ok}}
+          charlie -> {:halt, {:halt, alpha * bravo * charlie}}
+        end
+      end)
+    end)
   end
 end
