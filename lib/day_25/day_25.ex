b481855f2f1c437296bedb4ec82ec35d89b16b66
Andrew Stewart <andrew@stwrt.ca>
2020-11-30 20:56:47 -0800

    Pre-generate code scaffolding for each day

---
+++ lib/day_25/day_25.ex
@@ -0,0 +1,24 @@
+defmodule Day25 do
+  @moduledoc false
+
+  def run do
+    part1()
+    part2()
+  end
+
+  @data [__DIR__, "DATA"]
+        |> Path.join()
+        |> File.read!()
+
+  def data do
+    @data
+  end
+
+  def part1 do
+    data()
+  end
+
+  def part2 do
+    data()
+  end
+end
