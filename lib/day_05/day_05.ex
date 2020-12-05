be668c70aee2d83f7201ca9ae9ec97546700649b
Andrew Stewart <andrew@stwrt.ca>
2020-12-04 22:04:15 -0800

    Add Day 05 solution, data

--- lib/day_05/day_05.ex
+++ lib/day_05/day_05.ex
@@ -1,6 +1,9 @@
 defmodule Day05 do
   @moduledoc false
 
+  @rows 0..127
+  @seats 0..7
+
   def run do
     part1()
     part2()
@@ -9,17 +12,54 @@ defmodule Day05 do
   @data [__DIR__, "DATA"]
         |> Path.join()
         |> File.stream!()
-        |> Enum.to_list()
+        |> Stream.map(&String.trim/1)
+        |> Enum.map(fn <<row::binary-size(7), seat::binary>> -> {row, seat} end)
 
   def data do
-    @data
+    for {row, seat} <- @data,
+      into: MapSet.new(),
+      do: row_number(row) * 8 + seat_number(seat)
   end
 
+  # multiply row number by 8 and add column to get seat ID, find highest id.
   def part1 do
-    data()
+    Enum.max(data())
   end
 
+  # find your seat id -- it is _not_ present in the list but has immediate neighbours (ID +1, ID -1)
   def part2 do
-    data()
+    seats = data()
+
+    Enum.find_value(seats, fn id ->
+      if (id + 2 in seats) and not (id + 1 in seats) do
+        id + 1
+      end
+    end)
   end
+
+  defp split(range) do
+    Enum.split(range, div(Enum.count(range), 2))
+  end
+
+  defp front(path, range, continuation) do
+    case split(range) do
+      {[result], _} -> result
+      {front, _} -> continuation.(path, front)
+    end
+  end
+
+  defp back(path, range, continuation) do
+    case split(range) do
+      {_, [result]} -> result
+      {_, back} -> continuation.(path, back)
+    end
+  end
+
+  defp row_number(path, range \\ @rows)
+  defp row_number(<<"F", rest::binary>>, range), do: front(rest, range, &row_number/2)
+  defp row_number(<<"B", rest::binary>>, range), do: back(rest, range, &row_number/2)
+
+  defp seat_number(path, range \\ @seats)
+  defp seat_number(<<"L", rest::binary>>, range), do: front(rest, range, &seat_number/2)
+  defp seat_number(<<"R", rest::binary>>, range), do: back(rest, range, &seat_number/2)
 end
