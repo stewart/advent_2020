26d3ccc2bfa402f5ba139a74c9aa8b688347f1f1
Andrew Stewart <andrew@stwrt.ca>
2020-12-05 21:41:59 -0800

    Add answers and data for Day 06

--- lib/day_06/day_06.ex
+++ lib/day_06/day_06.ex
@@ -6,20 +6,34 @@ defmodule Day06 do
     part2()
   end
 
+  parse = fn input ->
+    for answers <- String.split(input, "\n", trim: true) do
+      answers |> String.graphemes() |> MapSet.new()
+    end
+  end
+
   @data [__DIR__, "DATA"]
         |> Path.join()
-        |> File.stream!()
-        |> Enum.to_list()
+        |> File.read!()
+        |> String.split("\n\n", trim: true)
+        |> Enum.map(parse)
 
   def data do
     @data
   end
 
+  # Count unique answers per group, count total.
   def part1 do
     data()
+    |> Enum.map(fn answers -> Enum.reduce(answers, &MapSet.union/2) end)
+    |> Enum.map(&MapSet.size/1)
+    |> Enum.sum()
   end
 
   def part2 do
     data()
+    |> Enum.map(fn answers -> Enum.reduce(answers, &MapSet.intersection/2) end)
+    |> Enum.map(&MapSet.size/1)
+    |> Enum.sum()
   end
 end
