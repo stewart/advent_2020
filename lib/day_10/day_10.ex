a9c83ef782864885dabd9aa2f7106ebc2b0c52ae
Andrew Stewart <andrew@stwrt.ca>
2020-12-09 22:11:07 -0800

    Add code, data for Day 10 solutions
    I dislike putting on my dynamic programming hat, but it was a fun excuse
    to use the `for` comprehension's ability to also function as a reducer.

--- lib/day_10/day_10.ex
+++ lib/day_10/day_10.ex
@@ -1,25 +1,43 @@
 defmodule Day10 do
   @moduledoc false
 
-  def run do
-    part1()
-    part2()
-  end
-
   @data [__DIR__, "DATA"]
         |> Path.join()
         |> File.stream!()
-        |> Enum.to_list()
+        |> Stream.map(&String.trim/1)
+        |> Stream.map(&String.to_integer/1)
+        |> Enum.sort()
 
   def data do
-    @data
+    data = @data
+    [0 | data] ++ [Enum.max(data) + 3]
   end
 
   def part1 do
-    data()
+    offsets = calculate_joltage_offsets(data())
+    offsets[1] * offsets[3]
   end
 
   def part2 do
-    data()
+    valid_joltage_combinations(data())
+  end
+
+  defp calculate_joltage_offsets(joltages) do
+    joltages
+    |> Enum.chunk_every(2, 1, :discard)
+    |> Enum.frequencies_by(fn [a, b] -> b - a end)
+  end
+
+  defp valid_joltage_combinations(joltages) do
+    Map.fetch!(calculate_offset_combinations(joltages), Enum.max(joltages))
+  end
+
+  defp calculate_offset_combinations(joltages) do
+    for joltage <- joltages, n <- 1..3, reduce: %{0 => 1} do
+      acc ->
+        acc
+        |> Map.put_new(joltage, 0)
+        |> Map.update!(joltage, & &1 + (acc[joltage - n] || 0))
+    end
   end
 end
