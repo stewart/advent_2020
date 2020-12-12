c07feb5e7caa51e2063e59bbf7e57e4125da474d
Andrew Stewart <andrew@stwrt.ca>
2020-12-12 11:49:58 -0800

    Day 11 + 12 code and data

--- lib/day_11/day_11.ex
+++ lib/day_11/day_11.ex
@@ -1,25 +1,88 @@
 defmodule Day11 do
   @moduledoc false
 
-  def run do
-    part1()
-    part2()
+  compile_coords = fn {input_line, y}, acc ->
+    chars = input_line |> String.split("", trim: true) |> Enum.with_index()
+
+    Enum.reduce(chars, acc, fn
+      {"#", x}, acc -> Map.put(acc, {x, y}, :taken)
+      {"L", x}, acc -> Map.put(acc, {x, y}, :empty)
+      {".", x}, acc -> Map.put(acc, {x, y}, :floor)
+    end)
   end
 
   @data [__DIR__, "DATA"]
         |> Path.join()
         |> File.stream!()
-        |> Enum.to_list()
+        |> Stream.map(&String.trim/1)
+        |> Enum.with_index()
+        |> Enum.reduce(%{}, compile_coords)
 
   def data do
     @data
   end
 
   def part1 do
-    data()
+    solve(&adjacent_taken_seats/2, 4)
   end
 
   def part2 do
+    solve(&visible_taken_seats/2, 5)
+  end
+
+  defp solve(visibility_fn, threshold) do
     data()
+    |> simulate_until_settled(visibility_fn, threshold)
+    |> taken_seat_count()
+  end
+
+  defp taken_seat_count(%{} = seat_map) do
+    Enum.count(seat_map, fn {_, state} -> state == :taken end)
+  end
+
+  defp simulate_until_settled(%{} = seat_map, searcher, threshold) do
+    cycle = Stream.cycle([true])
+
+    Enum.reduce_while(cycle, {seat_map, _prev = %{}}, fn
+      _, {seats, seats} -> {:halt, seats}
+      _, {seats, _prev} -> {:cont, {simulate(seats, searcher, threshold), seats}}
+    end)
+  end
+
+  def simulate(%{} = seat_map, searcher, threshold \\ 4) do
+    Map.new(seat_map, fn {seat, state} ->
+      case {state, searcher.(seat_map, seat)} do
+        {:empty, 0} -> {seat, :taken}
+        {:taken, n} when n >= threshold -> {seat, :empty}
+        _ -> {seat, state}
+      end
+    end)
+  end
+
+  def adjacent_taken_seats(%{} = seat_map, seat) do
+    Enum.count(neighbours(seat), & seat_map[&1] == :taken)
+  end
+
+  defp neighbours({x, y} = _seat) do
+    for dx <- -1..1, dy <- -1..1, {dx, dy} != {0, 0}, do: {x + dx, y + dy}
+  end
+
+  def visible_taken_seats(%{} = seat_map, seat) do
+    Enum.count(visible(seat_map, seat), & seat_map[&1] == :taken)
+  end
+
+  defp visible(%{} = seat_map, {x, y} = _seat) do
+    cycle = Stream.cycle([true])
+    directions = for dx <- -1..1, dy <- -1..1, {dx, dy} != {0, 0}, do: {dx, dy}
+
+    Enum.reduce(directions, [], fn {dx, dy}, acc ->
+      Enum.reduce_while(cycle, {acc, x + dx, y + dy}, fn _, {acc, ix, iy} ->
+        case Map.get(seat_map, {ix, iy}) do
+          :floor -> {:cont, {acc, ix + dx, iy + dy}}
+          nil -> {:halt, acc}
+          _ -> {:halt, [{ix, iy} | acc]}
+        end
+      end)
+    end)
   end
 end
--- lib/day_12/day_12.ex
+++ lib/day_12/day_12.ex
@@ -1,25 +1,77 @@
 defmodule Day12 do
   @moduledoc false
 
-  def run do
-    part1()
-    part2()
+  parse = fn <<action, value::binary>> ->
+    {action, String.to_integer(value)}
   end
 
   @data [__DIR__, "DATA"]
         |> Path.join()
         |> File.stream!()
-        |> Enum.to_list()
+        |> Stream.map(&String.trim/1)
+        |> Enum.map(parse)
+
+  defmodule Ship do
+    defstruct [:pos, :h]
+
+    alias __MODULE__
+
+    def new, do: %Ship{h: 90, pos: {0, 0}}
+
+    def abs_pos(%Ship{pos: {x, y}}), do: abs(x) + abs(y)
+
+    def instruct({?N, v}, ship), do: move(ship, {0, v})
+    def instruct({?S, v}, ship), do: move(ship, {0, -v})
+    def instruct({?E, v}, ship), do: move(ship, {v, 0})
+    def instruct({?W, v}, ship), do: move(ship, {-v, 0})
+    def instruct({?F, v}, ship), do: move_forward(ship, v)
+    def instruct({?R, v}, ship), do: adjust_heading(ship, v)
+    def instruct({?L, v}, ship), do: adjust_heading(ship, -v)
+
+    defp move(%Ship{pos: {x, y}} = ship, {dx, dy}), do: %Ship{ship | pos: {x + dx, y + dy}}
+
+    defp move_forward(%Ship{h: h} = ship, v) when h <= 45 or h > 315, do: move(ship, {0, v})
+    defp move_forward(%Ship{h: h} = ship, v) when h <= 135, do: move(ship, {v, 0})
+    defp move_forward(%Ship{h: h} = ship, v) when h <= 225, do: move(ship, {0, -v})
+    defp move_forward(%Ship{h: h} = ship, v) when h <= 315, do: move(ship, {-v, 0})
+
+    defp adjust_heading(%Ship{h: h} = ship, v), do: %Ship{ship | h: Integer.mod(h + v, 360)}
+  end
+
+  defmodule Waypoint do
+    defstruct [:pos, :h, :wp]
+
+    def new, do: %__MODULE__{h: 90, pos: {0, 0}, wp: {10, 1}}
+
+    def abs_pos(%__MODULE__{pos: {x, y}}), do: abs(x) + abs(y)
+
+    def instruct({?N, v}, ship), do: move_wp(ship, {0, v})
+    def instruct({?S, v}, ship), do: move_wp(ship, {0, -v})
+    def instruct({?E, v}, ship), do: move_wp(ship, {v, 0})
+    def instruct({?W, v}, ship), do: move_wp(ship, {-v, 0})
+    def instruct({?F, v}, ship), do: move_ship(ship, v)
+    def instruct({?R, v}, ship), do: adjust_wp_heading(ship, v)
+    def instruct({?L, v}, ship), do: adjust_wp_heading(ship, 360 - v)
+
+    defp move_wp(%__MODULE__{wp: {x, y}} = ship, {dx, dy}), do: %__MODULE__{ship | wp: {x + dx, y + dy}}
+    defp move_ship(%__MODULE__{pos: {x, y}, wp: {wx, wy}} = ship, n), do: %__MODULE__{ship | pos: {x + (n * wx), y + (n * wy)}}
+
+    defp adjust_wp_heading(%__MODULE__{wp: {x, y}} = ship, 90), do: %__MODULE__{ship | wp: {y, -x}}
+    defp adjust_wp_heading(%__MODULE__{wp: {x, y}} = ship, 180), do: %__MODULE__{ship | wp: {-x, -y}}
+    defp adjust_wp_heading(%__MODULE__{wp: {x, y}} = ship, 270), do: %__MODULE__{ship | wp: {-y, x}}
+  end
 
   def data do
     @data
   end
 
   def part1 do
-    data()
+    ship = Enum.reduce(data(), Ship.new(), &Ship.instruct/2)
+    Ship.abs_pos(ship)
   end
 
   def part2 do
-    data()
+    waypoint = Enum.reduce(data(), Waypoint.new(), &Waypoint.instruct/2)
+    Waypoint.abs_pos(waypoint)
   end
 end
