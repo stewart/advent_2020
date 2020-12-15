d2bbb981e3995a2306d582fcdc97d9a1676d6aca
Andrew Stewart <andrew@stwrt.ca>
2020-12-14 22:54:55 -0800

    Day 15 solutions

--- lib/day_15/day_15.ex
+++ lib/day_15/day_15.ex
@@ -1,25 +1,34 @@
 defmodule Day15 do
   @moduledoc false
 
-  def run do
-    part1()
-    part2()
-  end
-
-  @data [__DIR__, "DATA"]
-        |> Path.join()
-        |> File.stream!()
-        |> Enum.to_list()
-
   def data do
-    @data
+    [0, 13, 1, 16, 6, 17]
   end
 
   def part1 do
-    data()
+    {last, _} = play(data(), 2020)
+    last
   end
 
+  # this could probably be faster.
   def part2 do
-    data()
+    {last, _} = play(data(), 30_000_000)
+    last
+  end
+
+  defp play(starting_numbers, turns) do
+    Enum.reduce(1..turns, {nil, %{}}, fn
+      n, {_, acc} when n <= length(starting_numbers) ->
+        val = Enum.at(starting_numbers, n - 1)
+        {val, Map.update(acc, val, [n], & [n | &1])}
+
+      n, {prev, acc} ->
+        val = case acc do
+          %{^prev => [one, two | _]} -> one - two
+          _ -> 0
+        end
+
+        {val, Map.update(acc, val, [n], & [n | &1])}
+    end)
   end
 end
